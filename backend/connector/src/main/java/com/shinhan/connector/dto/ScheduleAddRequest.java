6c4d555b7450c3cb55606bdf4e768bdbe4595ecc
bmincof <bminlvcoffee@gmail.com>
2023-09-02 23:51:56 +0900

    Feat: toScheduleEntity 추가

--- backend/connector/src/main/java/com/shinhan/connector/dto/ScheduleAddRequest.java
+++ backend/connector/src/main/java/com/shinhan/connector/dto/ScheduleAddRequest.java
@@ -1,8 +1,13 @@
 package com.shinhan.connector.dto;
 
+import com.shinhan.connector.entity.Schedule;
+import com.shinhan.connector.enums.Alarm;
+import com.shinhan.connector.enums.RepeatCycle;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @NoArgsConstructor
@@ -22,4 +27,16 @@ public class ScheduleAddRequest {
     private String content;
     // 알람 여부
     private Integer alarm;
+    private String memberId;
+
+    public Schedule toScheduleEntity() {
+        return Schedule.builder()
+                .name(this.getName())
+                .content(this.getContent())
+                .category(this.getCategory())
+                .date(this.getDate())
+                .repeatCycle(RepeatCycle.getRepeatCycle(this.getRepeatCycle()))
+                .alarm(Alarm.getAlarm(this.getAlarm()))
+                .build();
+    }
 }
