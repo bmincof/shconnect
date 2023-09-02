8f68ddab32b5acd34d83d748b1aba4fd10086dec
Sohee-Kim-ln <livre6491@gmail.com>
2023-09-02 22:26:46 +0900

    Feat: Redux 베이스 코드 추가 (#1)

---
+++ frontend/shconnector/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import roodReducer from "./reducers/RootReducer";
+
+const store = configureStore({
+  reducer: roodReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export default store;
