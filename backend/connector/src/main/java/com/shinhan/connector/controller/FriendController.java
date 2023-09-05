f5a3816bfd00241c91b21a26735be4a1fb1e9d2c
Duhui-Kim <dadada022863@gmail.com>
2023-09-05 21:06:09 +0900

    Feat: 지인 목록 조회 및 상세 조회 기능 구현

--- backend/connector/src/main/java/com/shinhan/connector/controller/FriendController.java
+++ backend/connector/src/main/java/com/shinhan/connector/controller/FriendController.java
@@ -3,15 +3,15 @@ package com.shinhan.connector.controller;
 import com.shinhan.connector.config.jwt.UserDetailsImpl;
 import com.shinhan.connector.dto.FriendAddRequest;
 import com.shinhan.connector.dto.FriendAddResponse;
+import com.shinhan.connector.dto.FriendResponse;
 import com.shinhan.connector.service.FriendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +22,15 @@ public class FriendController {
     public ResponseEntity<FriendAddResponse> createFriend(@RequestBody FriendAddRequest friendAddRequest, @AuthenticationPrincipal UserDetailsImpl user) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(friendService.createFriend(friendAddRequest, user));
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<FriendResponse>> getFriends(@AuthenticationPrincipal UserDetailsImpl user) {
+        return ResponseEntity.ok(friendService.getAll(user));
+    }
+
+    @GetMapping("/{friendNo}")
+    public ResponseEntity<FriendResponse> getFriend(@PathVariable Integer friendNo, @AuthenticationPrincipal UserDetailsImpl user) {
+        return ResponseEntity.ok(friendService.getFriend(friendNo, user));
     }
 }
--- backend/connector/src/main/java/com/shinhan/connector/service/FriendService.java
+++ backend/connector/src/main/java/com/shinhan/connector/service/FriendService.java
@@ -3,13 +3,19 @@ package com.shinhan.connector.service;
 import com.shinhan.connector.config.jwt.UserDetailsImpl;
 import com.shinhan.connector.dto.FriendAddRequest;
 import com.shinhan.connector.dto.FriendAddResponse;
+import com.shinhan.connector.dto.FriendResponse;
 import com.shinhan.connector.entity.Friend;
 import com.shinhan.connector.entity.Member;
 import com.shinhan.connector.repository.FriendRepository;
 import com.shinhan.connector.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -29,5 +35,22 @@ public class FriendService {
 
         log.info("[지인 등록] 지인 등록 완료");
         return new FriendAddResponse(friend);
+    }
+
+    public List<FriendResponse> getAll(UserDetailsImpl user) {
+        log.info("[지인목록 조회] 지인목록 조회 요청. {}", user.toString());
+
+        return friendRepository.findByMember(user.getId()).stream()
+                .map((friend -> FriendResponse.fromEntity(friend)))
+                .collect(Collectors.toList());
+    }
+
+    public FriendResponse getFriend(Integer friendNo, UserDetailsImpl user) {
+        log.info("[지인상세 조회] 지인 상세 조회 시작. {}, {}", friendNo, user.toString());
+
+        return FriendResponse.fromEntity(friendRepository.findById(friendNo).orElseThrow(() -> {
+            log.error("[지인상세 조회] 지인 번호 잘못됨");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "해당하는 친구가 없습니다.");
+        }));
     }
 }
