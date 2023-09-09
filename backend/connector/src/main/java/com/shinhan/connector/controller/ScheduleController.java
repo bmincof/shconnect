b30f550cdf0083fe7fbe00a7ea80898a832b35c3
bmincof <bminlvcoffee@gmail.com>
2023-09-09 17:22:24 +0900

    Feat: 일정 목록 조회 API 추가

--- backend/connector/src/main/java/com/shinhan/connector/controller/ScheduleController.java
+++ backend/connector/src/main/java/com/shinhan/connector/controller/ScheduleController.java
@@ -1,15 +1,16 @@
 package com.shinhan.connector.controller;
 
-import com.shinhan.connector.dto.ResponseMessage;
-import com.shinhan.connector.dto.ScheduleAddRequest;
-import com.shinhan.connector.dto.ScheduleAddResponse;
-import com.shinhan.connector.dto.ScheduleResponse;
+import com.shinhan.connector.config.jwt.UserDetailsImpl;
+import com.shinhan.connector.dto.*;
 import com.shinhan.connector.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/schedule")
@@ -37,5 +38,13 @@ public class ScheduleController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(scheduleService.selectSchedule(scheduleNo));
+    }
+
+    // 일정 목록 조회
+    @GetMapping("/list")
+    public ResponseEntity<List<ScheduleListResponse>> getSchedules(@AuthenticationPrincipal UserDetailsImpl user) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(scheduleService.selectAllSchedule(user));
     }
 }
