2e0cee7d3ee90e08513dcea915c9430dc1c8a8fd
bmincof <bminlvcoffee@gmail.com>
2023-09-09 16:48:05 +0900

    Feat: 일정상세조회 API 추가

--- backend/connector/src/main/java/com/shinhan/connector/controller/ScheduleController.java
+++ backend/connector/src/main/java/com/shinhan/connector/controller/ScheduleController.java
@@ -3,8 +3,10 @@ package com.shinhan.connector.controller;
 import com.shinhan.connector.dto.ResponseMessage;
 import com.shinhan.connector.dto.ScheduleAddRequest;
 import com.shinhan.connector.dto.ScheduleAddResponse;
+import com.shinhan.connector.dto.ScheduleResponse;
 import com.shinhan.connector.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,5 +29,13 @@ public class ScheduleController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(scheduleService.deleteSchedule(scheduleNo));
+    }
+
+    // 일정 상세 조회
+    @GetMapping("/{scheduleNo}")
+    public ResponseEntity<ScheduleResponse> getScheduleDetail(@PathVariable Integer scheduleNo) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(scheduleService.selectSchedule(scheduleNo));
     }
 }
