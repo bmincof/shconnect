1021d4963726d02a47f4ac6eb8de567c2ddb9b9c
bmincof <bminlvcoffee@gmail.com>
2023-09-09 21:04:17 +0900

    Feat: 선물 레포지터리 추가

---
+++ backend/connector/src/main/java/com/shinhan/connector/repository/GiftReceiveRepository.java
@@ -0,0 +1,7 @@
+package com.shinhan.connector.repository;
+
+import com.shinhan.connector.entity.GiftReceive;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GiftReceiveRepository extends JpaRepository<GiftReceive, Integer> {
+}
---
+++ backend/connector/src/main/java/com/shinhan/connector/repository/GiftSendRepository.java
@@ -0,0 +1,7 @@
+package com.shinhan.connector.repository;
+
+import com.shinhan.connector.entity.GiftSend;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GiftSendRepository extends JpaRepository<GiftSend, Integer> {
+}
