65e21e0f3d60ccc30246f9f417c64a4efe217a8e
bmincof <bminlvcoffee@gmail.com>
2023-09-10 15:11:49 +0900

    Feat: 선물 등록 응답 DTO 수정

--- backend/connector/src/main/java/com/shinhan/connector/dto/GiftAddResponse.java
+++ backend/connector/src/main/java/com/shinhan/connector/dto/GiftAddResponse.java
@@ -1,5 +1,7 @@
 package com.shinhan.connector.dto;
 
+import com.shinhan.connector.entity.GiftReceive;
+import com.shinhan.connector.entity.GiftSend;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,8 +12,18 @@ public class GiftAddResponse {
     private Integer scheduleNo;
     private String name;
     private String category;
-    private Integer price;
+    private Long price;
     private String note;
 
-
+    // GiftSend 엔티티로부터 응답 DTO를 생성
+    public static GiftAddResponse fromGiftSendEntity(GiftSend giftSend) {
+        return GiftAddResponse.builder()
+                .giftNo(giftSend.getNo())
+                .scheduleNo(giftSend.getSchedule().getNo())
+                .name(giftSend.getName())
+                .category(giftSend.getCategory())
+                .price(giftSend.getPrice())
+                .note(giftSend.getNote())
+                .build();
+    }
 }
