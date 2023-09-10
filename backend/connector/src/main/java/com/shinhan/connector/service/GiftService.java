dd8daeea8c27c91da809b29e8aee60b86c748210
bmincof <bminlvcoffee@gmail.com>
2023-09-10 20:25:16 +0900

    Feat: 보낸 선물 조회 로직 구현

--- backend/connector/src/main/java/com/shinhan/connector/service/GiftService.java
+++ backend/connector/src/main/java/com/shinhan/connector/service/GiftService.java
@@ -3,8 +3,10 @@ package com.shinhan.connector.service;
 import com.shinhan.connector.config.jwt.UserDetailsImpl;
 import com.shinhan.connector.dto.GiftAddRequest;
 import com.shinhan.connector.dto.GiftAddResponse;
+import com.shinhan.connector.dto.GiftSendResponse;
 import com.shinhan.connector.dto.ResponseMessage;
 import com.shinhan.connector.entity.GiftSend;
+import com.shinhan.connector.entity.Schedule;
 import com.shinhan.connector.repository.GiftReceiveRepository;
 import com.shinhan.connector.repository.GiftSendRepository;
 import com.shinhan.connector.repository.ScheduleRepository;
@@ -13,7 +15,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -49,6 +53,32 @@ public class GiftService {
         if (option.equals("give")) {
             giftSendRepository.deleteById(giftNo);
             return new ResponseMessage("삭제가 완료되었습니다.");
+        } else if (option.equals("receive")) {
+            return null;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public GiftSendResponse getGift(Integer giftNo, String option) {
+        if (option.equals("give")) {
+            return GiftSendResponse.fromEntity(giftSendRepository.findById(giftNo).orElseThrow(NoSuchElementException::new));
+        } else if (option.equals("receive")) {
+            return null;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<GiftSendResponse> getAllGift(String option, Integer friendNo, UserDetailsImpl user) {
+        if (option.equals("give")) {
+            // 회원의 일정 목록에 있는 모든 보낸선물을 하나의 리스트로 담기
+            return scheduleRepository.findByMember(user.getId()).stream()
+                    .flatMap(schedule -> schedule.getGiftSends().stream())
+                    .map(GiftSendResponse::fromEntity)
+                    .collect(Collectors.toList());
         } else if (option.equals("receive")) {
             return null;
         } else {
