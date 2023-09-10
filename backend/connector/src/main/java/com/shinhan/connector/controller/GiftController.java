0b6b37f18ff176699c086d8d8d023f76d7f6f93a
bmincof <bminlvcoffee@gmail.com>
2023-09-10 15:12:40 +0900

    Feat: 선물 등록 API 추가

--- backend/connector/src/main/java/com/shinhan/connector/controller/GiftController.java
+++ backend/connector/src/main/java/com/shinhan/connector/controller/GiftController.java
@@ -1,11 +1,27 @@
 package com.shinhan.connector.controller;
 
+import com.shinhan.connector.config.jwt.UserDetailsImpl;
+import com.shinhan.connector.dto.GiftAddRequest;
+import com.shinhan.connector.dto.GiftAddResponse;
+import com.shinhan.connector.service.GiftService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/gift")
 public class GiftController {
+    private final GiftService giftService;
+
+    @PostMapping
+    public ResponseEntity<GiftAddResponse> createGift(@RequestBody GiftAddRequest giftAddRequest,
+                                                      @RequestParam String option,
+                                                      @AuthenticationPrincipal UserDetailsImpl user) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(giftService.createGift(giftAddRequest, option, user));
+    }
 }
